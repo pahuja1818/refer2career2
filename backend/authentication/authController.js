354fb4b58f84033a75f436321eb9e80e6d3dd177
pahuja1818 <tusharpahuja510@gmail.com>
2021-01-09 16:03:15 +0530

    fixed

--- backend/authentication/authController.js
+++ backend/authentication/authController.js
@@ -3,7 +3,6 @@ const bcrypt = require("bcrypt");
 const { body, validationResult } = require("express-validator");
 const users = require('../app.js');
 var nodemailer = require('nodemailer');
-const { htmlPrefilter } = require("jquery");
 var MongoClient = require('mongodb').MongoClient;
 var uri = "mongodb://pahuja_ji:1357902468@cluster0-shard-00-00.j00fc.mongodb.net:27017,cluster0-shard-00-01.j00fc.mongodb.net:27017,cluster0-shard-00-02.j00fc.mongodb.net:27017/insta-jobs?ssl=true&replicaSet=atlas-2a4jen-shard-0&authSource=admin&retryWrites=true&w=majority";
 var mongoUtil = require('../db.js').getDb();
