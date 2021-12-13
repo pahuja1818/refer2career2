2c7c9c492248057542d62d7d68ef606b9c9a1dd5
pahuja1818 <tusharpahuja510@gmail.com>
2021-12-13 20:07:08 +0530

    random

--- backend/authentication/authRoute.js
+++ backend/authentication/authRoute.js
@@ -3,7 +3,7 @@ var express = require('express');
 const { check, validationResult } = require("express-validator");
 const router = express.Router();
 
-const { registerUser, verifyOTP, login, sendMail, create, updateAdminDetails, getDetails, update, find, insertOTP, verify } = require('./authController.js');
+const { registerUser,getMails, verifyOTP, login, sendMail, create, updateAdminDetails, getDetails, update, find, insertOTP, verify } = require('./authController.js');
 
 router.post("/register/candidate", registerUser);
 router.post("/verify/candidate", verifyOTP);
@@ -16,5 +16,6 @@ router.post("/find", find);
 router.post("/otpRequest", insertOTP);
 router.post("/verifyOTP", verify);
 router.post("/send-mail", sendMail);
+router.post("/get-mails", getMails);
 
 module.exports = router;
