a98845024914d2106a6b28cce5db2f0940a305ae
pahuja1818 <tusharpahuja510@gmail.com>
2022-02-06 20:32:19 +0530

    minor changes!

--- src/app/shared/services/organizations.service.ts
+++ src/app/shared/services/organizations.service.ts
@@ -15,7 +15,7 @@ export class OrganizationsService {
   public baseUrl = '';
 
   constructor(private http: HttpClient) {
-  this.baseUrl = window.location.host.includes('localhost') ?  'http://localhost:8084' :  'https://instajobapp.herokuapp.com' ;
+  this.baseUrl = window.location.host.includes('localhost') ?  'http://localhost:8084' :  '' ;
 
    }
 
